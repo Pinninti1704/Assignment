5da4bef0d989db3eaadbed70eccb7f886a9c37ee
Pinninti1704 <33980697+Pinninti1704@users.noreply.github.com>
2022-01-11 12:13:59 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require('socket.io');
+const path = require('path');
+const moment = require('moment');
+const io = socketio(server, {
+  allowRequest: (req, callback) => {
+    const noOriginHeader = req.headers.origin === undefined;
+    callback(null, noOriginHeader);
+  }
+});
+
+const indexRoutes = require('./routes/index');
+
+app.locals.moment = moment;
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  '/leaflet',
+  express.static(path.join(__dirname, '/node_modules/leaflet/'))
+);
+app.use(
+  '/chart',
+  express.static(path.join(__dirname, '/node_modules/chart.js/'))
+);
+app.use(
+  '/moment',
+  express.static(path.join(__dirname, '/node_modules/moment/'))
+);
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Make io accessible to our router
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+// Requiring routes
+app.use(indexRoutes);
+
+app.get('*', async (req, res) => {
+  return res.redirect('/');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
